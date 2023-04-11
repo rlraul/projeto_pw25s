d0b6b37d004920f14f9d38e45c8b1bb3e1f83f6b
raulc <raul.cardoso@outlook.com.br>
2023-04-10 21:09:59 -0300

    commit inicial

---
+++ src/test/java/br/edu/utfpr/pw25s/projetoFinal/ProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pw25s.projetoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
